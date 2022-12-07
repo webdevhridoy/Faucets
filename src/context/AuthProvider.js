035dca12420e983c6e139b6dd66d4c47d0f440b3
md rafiqul islam <rafiqulislam11002@gmail.com>
2022-12-07 16:20:24 +0600

    workin on authenticatione

--- src/context/AuthProvider.js
+++ src/context/AuthProvider.js
@@ -1,5 +1,5 @@
 import React, { createContext, useEffect, useState } from 'react';
-import { createUserWithEmailAndPassword, getAuth, GoogleAuthProvider, onAuthStateChanged, signInWithEmailAndPassword, signInWithPopup, signOut, updateProfile } from 'firebase/auth';
+import { createUserWithEmailAndPassword, getAuth, GoogleAuthProvider, onAuthStateChanged, signInWithEmailAndPassword, signInWithPopup, signOut } from 'firebase/auth';
 import app from './firebase/firebase.config';
 
 
@@ -9,7 +9,6 @@ const AuthProvider = ({ children }) => {
     const [select, setSelect] = useState("");
 
     const [user, setUser] = useState('');
-    const [loading, setLoading] = useState(true);
     const googleProvider = new GoogleAuthProvider();
 
 
@@ -18,28 +17,22 @@ const AuthProvider = ({ children }) => {
     };
 
     const signIn = (email, password) => {
-        setLoading(true);
+
         return signInWithEmailAndPassword(auth, email, password);
     };
 
     const providerLogin = () => {
-        setLoading(true);
         return signInWithPopup(auth, googleProvider);
     };
 
     const logOut = () => {
-        setLoading(true);
         return signOut(auth);
-    };
-
-    const updateUser = (profile) => {
-        return updateProfile(auth.currentUser, profile);
     };
 
     useEffect(() => {
         const unsubscribe = onAuthStateChanged(auth, currentUser => {
             setUser(currentUser);
-            setLoading(false);
+
         });
         return () => unsubscribe();
     }, []);
@@ -47,11 +40,9 @@ const AuthProvider = ({ children }) => {
     const authInfo = {
         createUser,
         user,
-        loading,
         logOut,
         signIn,
         providerLogin,
-        updateUser,
         select,
         setSelect
     };
